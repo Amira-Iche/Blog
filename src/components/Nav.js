e4e9fef786f86c3bd2d78a046f4553d41dc2437c
Amira Iche <icheamira@gmail.com>
2022-06-04 20:06:48 +0300

    redesigned the nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
-// import logo from "../../public/logo192.png"
-
+import logo from "../media/logo1.jpg";
+import { AiOutlineHome  } from 'react-icons/ai';
+import {  BsCardList ,BsPencilSquare } from 'react-icons/bs';
 const Navbar = () => {
 
     return (
         <header>
              <nav className="navbar">  
-             <img src="../logo192/png" alt="" />
+             <img src={logo} alt="" />
             <ul className="Navigation">
                 
-               <li> <Link to="/">Home</Link> </li>
-               <li> <Link to="/create">Add article</Link> </li>
+               <li> <Link to="/"><AiOutlineHome/> Home</Link> </li>
+               <li> <Link to="/"><BsCardList/> Your Travel Stories</Link> </li>
+               <li> <Link to="/create"><BsPencilSquare/>Write a Story</Link> </li>
+
             </ul>
       
-                
+        
              </nav>
         </header>
 
